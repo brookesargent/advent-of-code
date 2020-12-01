249cc01603a2f7f59cd346487266a57f6284f12a
brookesargent <brookesargent01@gmail.com>
2020-12-01 12:45:55 -0500

    remove parameter from both subset functions

--- 1/main.go
+++ 1/main.go
@@ -12,17 +12,17 @@ func main() {
 		log.Println(err)
 	}
 
-	subset1 := subsetSumPartOne(lines, len(lines), 2020)
-	subset2 := subsetSumPartTwo(lines, len(lines), 2020)
+	subset1 := subsetSumPartOne(lines, 2020)
+	subset2 := subsetSumPartTwo(lines, 2020)
 	fmt.Println("The answer is: " + strconv.Itoa(subset1[0] * subset1[1]))
 	fmt.Println("The answer is: " + strconv.Itoa(subset2[0] * subset2[1] * subset2[2]))
 
 }
 
-func subsetSumPartOne(numbers []int, setLength int, sum int) []int {
+func subsetSumPartOne(numbers []int, sum int) []int {
 	var subset []int
-	for i := 0; i < setLength; i++ {
-		for j:= 1; j < setLength; j++ {
+	for i := 0; i < len(numbers); i++ {
+		for j:= 1; j < len(numbers); j++ {
 			if numbers[i] +  numbers[j] == sum {
 				subset = append(subset, numbers[i], numbers[j])
 			}
@@ -32,11 +32,11 @@ func subsetSumPartOne(numbers []int, setLength int, sum int) []int {
 	return subset
 }
 
-func subsetSumPartTwo(numbers []int, setLength int, sum int) []int {
+func subsetSumPartTwo(numbers []int, sum int) []int {
 	var subset []int
-	for i := 0; i < setLength; i++ {
-		for j:= 1; j < setLength; j++ {
-			for k := 2; k < setLength; k++ {
+	for i := 0; i < len(numbers); i++ {
+		for j:= 1; j < len(numbers); j++ {
+			for k := 2; k < len(numbers); k++ {
 				if numbers[i] +  numbers[j] + numbers[k] == sum {
 					subset = append(subset, numbers[i], numbers[j], numbers[k])
 				}
