51d4d4d43e925c9c34b26da86df122bcefe9bf8e
brookesargent <brookesargent01@gmail.com>
2020-12-05 12:18:04 -0500

    day 3 part 2 solution with part 1 rework

--- 3/main.go
+++ 3/main.go
@@ -10,37 +10,78 @@ import (
 	"github.com/brookesargent/advent-of-code2020/helper"
 )
 
+type Slope struct {
+	Right int
+	Down  int
+}
+
 func main() {
 	start := time.Now()
-	position := 0
-	trees := 0
+
 	lines, err := helper.ReadInputTxtToStringSlice("3/input.txt")
 	if err != nil {
 		log.Println(err)
 	}
 
+	// part one
+	slopeA := Slope{
+		Right: 3,
+		Down:  1,
+	}
+	treesA := calculateSlope(lines, slopeA)
+
+	// part two
+	var treesB []int
+	slopeB := []Slope{
+		{Right: 1, Down: 1},
+		{Right: 3, Down: 1},
+		{Right: 5, Down: 1},
+		{Right: 7, Down: 1},
+		{Right: 1, Down: 2},
+	}
+
+	for _, slope := range slopeB {
+		treesB = append(treesB, calculateSlope(lines, slope))
+	}
+
+	product := treesB[0] * treesB[1] * treesB[2] * treesB[3] * treesB[4]
+
+	fmt.Printf("Part 1: There are %s trees\n", strconv.Itoa(treesA))
+	fmt.Printf("Part 2: There are %s trees\n", strconv.Itoa(product))
+	fmt.Println("Program duration: " + time.Since(start).String())
+}
+
+func calculateSlope(lines []string, slope Slope) int {
+	xPosition := 0
+	yPosition := 0
+	trees := 0
 	for i := 0; i < len(lines); i++ {
 		if i > 0 {
-			row := strings.Split(lines[i], "")
-			if row[position] == "#" {
-				trees++
+			// check for a tree if we have traversed down enough, otherwise keep traversing
+			if yPosition == slope.Down {
+				row := strings.Split(lines[i], "")
+				if row[xPosition] == "#" {
+					trees++
+				}
+				yPosition = 0
+			} else {
+				yPosition++
+				continue
 			}
 		}
 
-		switch (len(lines[i]) - 1) - position {
-		case 0:
-			position = 2
-		case 1:
-			position = 1
-		case 2:
-			position = 0
-		default:
-			position += 3
+		//move right
+		rightMoves := (len(lines[i]) - 1) - xPosition
+		if rightMoves < slope.Right {
+			xPosition = (slope.Right - 1) - rightMoves
+		} else {
+			xPosition += slope.Right
 		}
 
+		//move down
+		yPosition++
 		continue
 	}
 
-	fmt.Printf("There are %s trees\n", strconv.Itoa(trees))
-	fmt.Println("Program duration: " + time.Since(start).String())
+	return trees
 }
