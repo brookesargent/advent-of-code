fe4b9be417cbc42c60005879290a6bc3049d2df6
brookesargent <brookesargent01@gmail.com>
2020-12-08 19:36:55 -0500

    day 8 part 2 solution + cleanup

--- 8/main.go
+++ 8/main.go
@@ -25,34 +25,32 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-	moveList := formatGameMoves(lines)
-	accumulator, idx := 0, 0
-	for {
-		currentMove := moveList[idx]
-		if currentMove.Used {
-			break
-		} else {
-			moveList[idx].Used = true
+	moves1 := formatGameMoves(lines)
+	moves2 := append([]GameMove(nil), moves1...)
+
+	// part 1
+	accumulator1, _ := runBootCode(moves1)
+
+	// part 2
+	var accumulator2 int
+	var programComplete bool
+	for i, move := range moves2 {
+		currentMoves := append([]GameMove(nil), moves2...)
+		if move.Type == "jmp" {
+			currentMoves[i].Type = "nop"
+			accumulator2, programComplete = runBootCode(currentMoves)
+		} else if move.Type == "nop" {
+			currentMoves[i].Type = "jmp"
+			accumulator2, programComplete = runBootCode(currentMoves)
 		}
 
-		if currentMove.Type == "nop" {
-			idx++
-		} else if currentMove.Type == "acc" {
-			if currentMove.Direction == "+" {
-				accumulator += currentMove.Count
-			} else if currentMove.Direction == "-" {
-				accumulator -= currentMove.Count
-			}
-			idx++
-		} else if currentMove.Type == "jmp" {
-			if currentMove.Direction == "+" {
-				idx += currentMove.Count
-			} else if currentMove.Direction == "-" {
-				idx -= currentMove.Count
-			}
+		if programComplete {
+			break
 		}
 	}
-	fmt.Println("Accumulator value: " + strconv.Itoa(accumulator))
+
+	fmt.Println("Accumulator value: " + strconv.Itoa(accumulator1))
+	fmt.Println("Accumulator value: " + strconv.Itoa(accumulator2))
 	fmt.Println("Program duration: " + time.Since(start).String())
 }
 
@@ -80,6 +78,44 @@ func formatGameMoves(lines []string) []GameMove {
 		})
 	}
 	return moveList
+}
+
+func runBootCode(moves []GameMove) (int, bool) {
+	accumulator, idx := 0, 0
+	programComplete := false
+
+	for {
+		currentMove := moves[idx]
+		if currentMove.Used {
+			break
+		} else {
+			moves[idx].Used = true
+		}
+
+		if currentMove.Type == "nop" {
+			idx++
+		} else if currentMove.Type == "acc" {
+			if currentMove.Direction == "+" {
+				accumulator += currentMove.Count
+			} else if currentMove.Direction == "-" {
+				accumulator -= currentMove.Count
+			}
+			idx++
+		} else if currentMove.Type == "jmp" {
+			if currentMove.Direction == "+" {
+				idx += currentMove.Count
+			} else if currentMove.Direction == "-" {
+				idx -= currentMove.Count
+			}
+		}
+
+		if idx >= len(moves) {
+			programComplete = true
+			break
+		}
+	}
+
+	return accumulator, programComplete
 }
 
 func trimFirstRune(s string) string {
