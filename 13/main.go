6c07a5cb3ee120ec785196267f5157df5d95d282
brookesargent <brookesargent01@gmail.com>
2020-12-13 22:16:04 -0500

    day 13 part 2 solution

--- 13/main.go
+++ 13/main.go
@@ -11,6 +11,11 @@ import (
 	"github.com/brookesargent/advent-of-code2020/helper"
 )
 
+type Bus struct {
+	ID     int
+	Offset int
+}
+
 func main() {
 	start := time.Now()
 
@@ -19,24 +24,78 @@ func main() {
 		log.Println(err)
 	}
 
+	// part one
 	departureTimestamp, err := strconv.Atoi(lines[0])
 	if err != nil {
 		log.Printf("%s is not a valid number: %s\n", lines[0], err)
 	}
+	earliestDeparture, busToTake := findEarliestDeparture(departureTimestamp, strings.Split(lines[1], ","))
+	d := (earliestDeparture - departureTimestamp) * busToTake
+	// part two
+	buses := formatBusIds(lines[1])
+	t := findT(buses)
+	fmt.Println("Part one: " + strconv.Itoa(d))
+	fmt.Println("Part two: " + strconv.Itoa(t))
+	fmt.Println("Program duration: " + time.Since(start).String())
+}
 
-	busIds := strings.Split(lines[1], ",")
-
+func findEarliestDeparture(timestamp int, busIds []string) (int, int) {
 	earliestDeparture := 0
 	busToTake := 0
 	for _, id := range busIds {
 		currentID, _ := strconv.Atoi(id)
-		d := float64(departureTimestamp) / float64(currentID)
+		d := float64(timestamp) / float64(currentID)
 		departure := int(math.Ceil(d)) * currentID
-		if departure >= departureTimestamp && departure < earliestDeparture || earliestDeparture == 0 {
+		if departure >= timestamp && departure < earliestDeparture || earliestDeparture == 0 {
 			earliestDeparture = departure
 			busToTake = currentID
 		}
 	}
-	fmt.Println((earliestDeparture - departureTimestamp) * busToTake)
-	fmt.Println("Program duration: " + time.Since(start).String())
+	return earliestDeparture, busToTake
+}
+
+func findT(buses []Bus) int {
+	t := 0
+	busCount := 2
+	offset := buses[0].ID
+	// works to find starting t - how do i make it work for 3, 4, 5 etc buses?
+	for busCount <= len(buses) {
+	count:
+		for i := t; true; i += offset {
+			for j := 0; j < busCount; j++ {
+				if (i+buses[j].Offset)%buses[j].ID != 0 {
+					break
+				} else if j == busCount-1 {
+					t = i
+					break count
+				}
+			}
+		}
+		offset = getOffset(busCount, buses)
+		busCount++
+	}
+	return t
+}
+
+func getOffset(busCount int, buses []Bus) int {
+	offset := 1
+	for i := 0; i < busCount; i++ {
+		offset *= buses[i].ID
+	}
+	return offset
+}
+
+func formatBusIds(line string) []Bus {
+	split := strings.Split(line, ",")
+	buses := make([]Bus, 0)
+	for i, v := range split {
+		id, err := strconv.Atoi(v)
+		if err == nil {
+			buses = append(buses, Bus{
+				ID:     id,
+				Offset: i,
+			})
+		}
+	}
+	return buses
 }
